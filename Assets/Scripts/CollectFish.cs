1030599b3524dfd8790b47a8a5feefab2ebd760e
Rob Godfrey <rob_godfrey@outlook.com>
2024-02-12 00:30:32 -1000

    Update movement

--- Assets/Scripts/CollectFish.cs
+++ Assets/Scripts/CollectFish.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 public class CollectFish : MonoBehaviour
 {
     [SerializeField] private PenguinController penguinController;
-    [SerializeField] private int fishValue = 1;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
         {
-            penguinController.IncrementFishCount(fishValue);
+            penguinController.IncrementFishCount();
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/PenguinController.cs
+++ Assets/Scripts/PenguinController.cs
@@ -7,7 +7,6 @@ using TMPro;
 
 public class PenguinController : MonoBehaviour
 {
-    [SerializeField] private float horizontalSpeed = 1f;
     [SerializeField] private TextMeshProUGUI fishCountText;
 
     [Header("Health Bar")]
@@ -17,6 +16,10 @@ public class PenguinController : MonoBehaviour
     [SerializeField] private Sprite healthBarBgRed;
     
     private Rigidbody rb;
+    private Collider playerCollider;
+    private PhysicMaterial physicsMaterial;
+    private float previousPositionX;
+
     private int playerHealth = 100;
     private int fishCount = 0;
     private int flashNum = 0; // to delay health bar flashing
@@ -24,19 +27,38 @@ public class PenguinController : MonoBehaviour
 
     void Start()
     {
+        playerCollider = GetComponent<Collider>();
+        physicsMaterial = playerCollider.material;
         rb = GetComponent<Rigidbody>();
+        previousPositionX = transform.position.x;
     }
 
     void FixedUpdate()
     {
+        float distanceTraveledX = Math.Abs(transform.position.x - previousPositionX);
+        float currentSpeedX = distanceTraveledX / Time.deltaTime;
+        previousPositionX = transform.position.x;
+
         // left/right controls
         float horizontalInputSpeed = Input.GetAxis("Horizontal");
-        rb.AddForce(Vector3.forward * horizontalInputSpeed * horizontalSpeed, ForceMode.VelocityChange);
+
+        if (horizontalInputSpeed != 0)
+        {
+            rb.AddForce(Vector3.forward * horizontalInputSpeed, ForceMode.VelocityChange);
+            physicsMaterial.dynamicFriction = 0.25f;
+
+            float rotationX = horizontalInputSpeed * 60f * Time.deltaTime;
+            transform.Rotate(Vector3.right, rotationX, Space.Self);
+        }
+        else
+        {
+            physicsMaterial.dynamicFriction = 0;
+        }
 
         // restrict max z axis velocity
-        if (new Vector3(0, 0, rb.velocity.z).magnitude > 10.0f)
+        if (new Vector3(0, 0, rb.velocity.z).magnitude > (currentSpeedX / 2))
         {
-            rb.velocity = new Vector3(0, 0, rb.velocity.z).normalized * 10.0f + new Vector3(rb.velocity.x, rb.velocity.y, 0);
+            rb.velocity = new Vector3(0, 0, rb.velocity.z).normalized * (currentSpeedX / 2) + new Vector3(rb.velocity.x, rb.velocity.y, 0);
         }
 
         // flash health bar if low
@@ -71,14 +93,13 @@ public class PenguinController : MonoBehaviour
             playerHealth -= 25;
             healthBarInner.fillAmount = playerHealth * 0.01f;
             hitRock = 0;
-            
         }
     }
 
-    public void IncrementFishCount(int fishValue)
+    public void IncrementFishCount()
     {
         Debug.Log("Got a fish :)");
-        fishCount += fishValue;
+        fishCount++;
         fishCountText.text = string.Format("{0}", fishCount);
     }
 }
